0ef01471e9629e64d93bb470a8807f6fa0424d4c
rezamoridi <rezamoridi2042@gmail.com>
2024-03-10 05:47:23 +0330

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+<h4>This is Learning Telegram Bot programming Project</h4>
+
+based on : telegram library
+
+bot name : gorbag
---
+++ main.py
@@ -0,0 +1,9 @@
+from typing import Final
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes, ApplicationHandlerStop
+
+
+TOKEN: Final = '6913958853:AAFLvOEJgY5dNKUgCyT84ag1FlfMiOvG_H0'
+BOT_USERNAME: Final = '@gorbagabot'
+
+
